5b47ea464abf743f5dfd43566e1d31861665b22f
Virgilio Cano <virgiliocano53@gmail.com>
2022-02-14 19:58:40 -0300

    feat: add redirect to PlanetsDetails page

---
+++ src/components/PlanetsDetails/PlanetsDetails.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+
+import './PlanetsDetails.css';
+import GridFilmsResidents from '../GridFilmsResidents';
+
+function PlanetsDetails() {
+  const { state } = useLocation();
+  const [values, setValues] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${state}`)
+      .then(resp => {
+        setValues([resp.data])
+      }).catch(error => {
+        console.log(error)
+      })
+  }, []);
+
+  const dataForGrid = {
+    header: [
+      'name',
+      'rotation_period',
+      'orbital_period',
+      'diameter',
+      'climate',
+      'gravity',
+      'terrain',
+      'surface_water',
+      'population'
+    ],
+    values,
+  }
+
+  return (
+    <div className='App'>
+      <h1>Planet Details</h1>
+      <GridFilmsResidents data={dataForGrid} />
+    </div>
+  );
+}
+
+export default PlanetsDetails;
